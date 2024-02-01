85c4cce94b7e29d71046cd145745247289a2f069
Cristian Valim <cristianvalim@pop-os.Home>
2024-02-01 19:31:29 +0000

    trying paths

---
+++ src/components/projetcs/index.jsx
@@ -0,0 +1,71 @@
+import { Container } from "./styles";
+import { pages } from '../../modules/pagesPath';
+import { useParams } from "react-router-dom";
+
+export function Project () {
+    const { slug } = useParams();
+
+    const projectsBySlug = Object.fromEntries(
+      Object.values(pages).flatMap(Object.values).map((project) => [project.slug, project])
+    );
+
+    console.log("projectsBySlug: ", projectsBySlug);
+
+    const findProjectBySlug = (slug) => projectsBySlug[slug] || null;
+
+    const project = findProjectBySlug(slug);
+
+    console.log("project: ", project);
+
+    if (!project) {
+      console.log(project, slug, projectsBySlug)
+      return <p>Projeto não encontrado</p>;
+    }
+
+    const {
+      title,
+      location,
+      builtArea,
+      year,
+      adtext,
+      description,
+      cover,
+      images,
+      figures,
+    } = project;
+
+    console.log("images: ", project.cover, project.images, project.figures);
+
+    return (
+      <Container>
+        <img className="cover" src={cover} alt="Capa do projeto" />
+  
+        <div className="flex-wrap">
+            <div className="description"><h1>{title}</h1>
+              <h2><span>{location ? "localização" : "conceito"}</span></h2>
+              <h2><span>{builtArea ? "área construída" : "projeto e modelagem tridimensional"}</span></h2>
+              <h2><span>{year ? "ano" : "execução"}</span></h2>
+              <h2><span></span></h2>
+        </div>  
+  
+            <div className="info">
+              <p>{adtext}</p>
+              <br/>
+              <p>{description}</p>
+            </div>
+        </div>
+  
+        <div className="gallery">
+          {images.map((image, index) => (
+            <img key={index} src={image} alt={`Foto ${index + 1}`} />
+          ))}
+        </div>
+  
+        <div className="figures">
+          {figures.map((figure, index) => (
+            <img key={index} src={figure} alt={`Figura ${index + 1}`} />
+          ))}
+        </div>
+      </Container>
+    );
+}
